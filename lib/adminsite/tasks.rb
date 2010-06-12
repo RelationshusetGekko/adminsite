fab96dc78c6dd53c683ea44644f58ad0c0e6a5d9
Liborio Cannici <lic@crd.dk>
2010-06-12 18:40:54 +0200

    Admin genrator rake task more secure in production

--- lib/adminsite/tasks.rb
+++ lib/adminsite/tasks.rb
@@ -6,11 +6,19 @@ namespace :adminsite do
     system "rsync -ruv #{migrate_dir} db"  
     system "rsync -ruv #{public_dir} ."  
   end
-  
+
   desc "Setup an admin account"
+  login = "admin"
+  if Rails.env.development?
+    password = "password"
+  else
+    password = Digest::MD5.hexdigest(Time.now.to_s)[0..5]
+  end
+  
   task :setup => :environment do
-    Admin.create!(:login                 => "admin",
-                  :password              => "password",
-                  :password_confirmation => "password")
+    Admin.create!(:login                 => login,
+                  :password              => password,
+                  :password_confirmation => password)
+    puts "Created administrator with login: #{login} and password: #{password}"
   end
 end
