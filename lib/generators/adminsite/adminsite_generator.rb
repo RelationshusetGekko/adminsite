c96c3e6df5df38caea968e813abc188a6457f46b
Benjamin Quorning <beq@crd.dk>
2011-01-07 11:44:34 +0100

    Thor command #template calls mkdir if needed. Thor command #remove_file instead of File.delete

--- lib/generators/adminsite/adminsite_generator.rb
+++ lib/generators/adminsite/adminsite_generator.rb
@@ -2,13 +2,9 @@ class AdminsiteGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
   def run_generation
     #append_file 'Rakefile', "require 'adminsite/tasks'"
-    FileUtils.mkdir_p 'lib/recipes'
     template 'application.rb', 'lib/recipes/application.rb'
-
-    FileUtils.mkdir_p 'app/views/admin/shared'
     template '_menu.html.haml', 'app/views/admin/shared/_menu.haml'
-
-    File.delete("public/index.html") if File.exist?("public/index.html")
+    remove_file 'public/index.html'
 
     rake "adminsite:sync",  :generate_only => true
     rake "db:migrate",      :generate_only => true
