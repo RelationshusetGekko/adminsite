92b42f70cfe495f26e4f8c40e92da36d11bc2bbc
Liborio Cannici <io@cannici.eu>
2011-01-07 11:14:20 +0100

    Fixed menu partial generation

--- lib/generators/adminsite/adminsite_generator.rb
+++ lib/generators/adminsite/adminsite_generator.rb
@@ -6,8 +6,8 @@ class AdminsiteGenerator < Rails::Generators::Base
     template 'application.rb', 'lib/recipes/application.rb'
     template 'routes.rb', 'config/routes.rb'
 
-    FileUtils.mkdir_p 'views/admin/shared'
-    template '_menu.html.haml', 'views/admin/shared/_menu.haml'
+    FileUtils.mkdir_p 'app/views/admin/shared'
+    template '_menu.html.haml', 'app/views/admin/shared/_menu.haml'
 
     File.delete("public/index.html") if File.exist?("public/index.html")
 
