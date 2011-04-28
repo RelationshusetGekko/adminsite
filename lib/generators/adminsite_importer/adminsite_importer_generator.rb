5ca576a5cacad8ff505a3f7c902b18f38b02d06c
Robin Wunderlin <row@crd.dk>
2011-04-28 16:59:13 +0200

    Add import generator

---
+++ lib/generators/adminsite_importer/adminsite_importer_generator.rb
@@ -0,0 +1,10 @@
+class AdminsiteImporterGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("../templates", __FILE__)
+
+  def run_generation
+    puts "Setting up Adminsite to import #{class_name} "
+
+    template  "klass_importer_task.rake", "lib/tasks/#{class_name.downcase}_importer.rake"
+
+  end
+end
