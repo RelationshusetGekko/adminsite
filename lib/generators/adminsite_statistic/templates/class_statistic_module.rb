e30f5057f013207dedd29834e53577bd31c3cfd0
Robin Wunderlin <row@crd.dk>
2011-09-26 22:02:22 +0200

    Add statistic generator

---
+++ lib/generators/adminsite_statistic/templates/class_statistic_module.rb
@@ -0,0 +1,32 @@
+module <%= class_name.titlecase %>::Statistic
+  class << self
+
+    def caching
+      return { :cache_for => cache_ttl } if cache_ttl_allowed?
+      {}
+    end
+
+    def cache_ttl
+      5.minutes
+    end
+
+    def cache_ttl_allowed?
+      # Only MemCache supports the :expires_in/:cache_for parameter
+      Rails.cache.is_a?(ActiveSupport::Cache::MemCacheStore)
+    end
+
+    def included(base)
+      base.instance_eval do
+
+        with_options <%= class_name.titlecase %>::Statistic.caching.merge(:count => :all) do |c|
+
+          # General Counts
+          c.define_statistic :all_count,                   :count => :all
+
+        end # with_options
+
+      end # base.instance_eval
+    end # included
+
+  end # class
+end
---
+++ lib/generators/adminsite_statistic/templates/class_statistics_helper.rb
@@ -0,0 +1,15 @@
+module Admin::<%= class_name.titlecase %>StatisticsHelper
+
+  def get_number(number, totalnumber)
+    percent = get_percent(number, totalnumber)
+    return "#{number} (#{percent}%)"
+  end
+
+  def get_percent(number, totalnumber)
+    return 0 if number.nil? || totalnumber.nil?
+    result = 0
+    result = ((number * 100.0) / totalnumber) unless totalnumber < 1
+    return sprintf("%.1f", result)
+  end
+
+end
