7d53a672c03a7175f43c213e3bdfbe6b6ac90147
Kassim Halloyta <kah@rhg.dk>
2015-09-08 10:28:21 +0200

    Added Show & Hide function To Filter

---
+++ app/assets/javascripts/adminsite/admin/hide_and_show.js
@@ -0,0 +1,11 @@
+function showSearchForm(){
+  $(".search_form form").show();
+  $(".search_form a#show").hide();
+  $(".search_form a#hide").show();
+}
+
+function  hideSearchForm(){
+  $(".search_form form").hide();
+  $(".search_form a#show").show();
+  $(".search_form a#hide").hide();
+}
