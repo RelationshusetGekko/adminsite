7e8a44ff1c9afd7ad5a3e4041b1792a05ea78048
Robin Wunderlin <row@crd.dk>
2015-10-09 15:32:45 +0200

    Add javascript_include_tag & stylesheet_link_tag as LiquidTags

--- lib/adminsite/liquid/tags/asset.rb
+++ lib/adminsite/liquid/tags/asset.rb
@@ -9,4 +9,28 @@ class Asset < Liquid::Tag
   end
 end
 
+class JavascriptIncludeTag < Liquid::Tag
+  def initialize(tag_name, javascript, tokens)
+    super
+    @javascript = javascript.strip
+  end
+
+  def render(context)
+    ActionController::Base.helpers.javascript_include_tag @javascript
+  end
+end
+
+class StylesheetLinkTag < Liquid::Tag
+  def initialize(tag_name, stylesheet, tokens)
+    super
+    @stylesheet = stylesheet.strip
+  end
+
+  def render(context)
+    ActionController::Base.helpers.stylesheet_link_tag @stylesheet
+  end
+end
+
 Liquid::Template.register_tag('asset', Asset)
+Liquid::Template.register_tag('javascript_include_tag', JavascriptIncludeTag)
+Liquid::Template.register_tag('stylesheet_link_tag', StylesheetLinkTag)
