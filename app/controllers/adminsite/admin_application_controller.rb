2de07f849b2204faa0f676e7bf64858c26a94408
Robin Wunderlin <row@crd.dk>
2016-04-12 14:23:44 +0200

    Fixed ability

--- app/controllers/adminsite/admin_application_controller.rb
+++ app/controllers/adminsite/admin_application_controller.rb
@@ -6,7 +6,13 @@ class Adminsite::AdminApplicationController < ActionController::Base
   helper :all, "admin/adminsite_application"
   layout 'adminsite/admin'
   before_filter :authenticate_adminsite_admin_user!
-  # authorize_resource
-  check_authorization
-  
+  authorize_resource
+  # check_authorization
+
+  protected
+
+  def current_ability
+    @current_ability ||= ::Adminsite::Ability.new(current_adminsite_admin_user)
+  end
+
 end
