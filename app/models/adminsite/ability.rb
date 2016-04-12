89e411259219f54caa9e2ff490db96486ad575fd
Bo Frederiksen <bof@rhg.dk>
2016-04-12 14:10:15 +0200

    Added the Ability class to adminsite.

---
+++ app/models/adminsite/ability.rb
@@ -0,0 +1,25 @@
+module Adminsite
+  class Ability
+    include CanCan::Ability
+
+    def initialize(user)
+      user ||= AdminUser.new
+      user.admin_user_roles.each do |role|
+        case role.to_sym
+        when :admin
+          can :manage, :all
+        when :editor
+          can :read, Adminsite::Page
+          can :read, Adminsite::PageLayout
+          can :read, Adminsite::FileAsset
+          can :read, Adminsite::AdminUser
+          can :read, Adminsite::AdminUserRole
+        when :customer
+          # can :read, Adminsite::Page
+          # can :read, Adminsite::PageLayout
+          # can :read, Adminsite::FileAsset
+        end
+      end
+    end
+  end
+end
