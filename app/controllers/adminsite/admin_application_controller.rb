28f731324111ee12215b795909030161d65db8dd
Bo Frederiksen <bof@rhg.dk>
2016-04-12 15:51:14 +0200

    Fixed authorize_resource

--- app/controllers/adminsite/admin_application_controller.rb
+++ app/controllers/adminsite/admin_application_controller.rb
@@ -8,18 +8,23 @@ class Adminsite::AdminApplicationController < ActionController::Base
   before_filter :authenticate_adminsite_admin_user!
 
   include ::CanCan::ControllerAdditions
-  authorize_resource class: :resource_class
-  check_authorization
+  authorize_resource class: lambda { |context| context.resource_class }
+  # check_authorization
 
   rescue_from CanCan::AccessDenied do |exception|
     render text: exception.message
   end
 
-  protected
+  def authorize!(action, subject, *args)
+    subject = subject.call(self) if subject.is_a?(Proc)
+    super(action, subject, *args)
+  end
 
   def resource_class
     self
   end
+
+  protected
 
   def current_ability
     @current_ability ||= ::Adminsite::Ability.new(current_adminsite_admin_user)
