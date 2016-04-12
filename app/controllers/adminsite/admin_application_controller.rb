228278932e9000db37755ff26f93a5f8b0a02386
Robin Wunderlin <row@crd.dk>
2016-04-12 14:34:01 +0200

    Fixed resource_class issue

--- app/controllers/adminsite/admin_application_controller.rb
+++ app/controllers/adminsite/admin_application_controller.rb
@@ -2,17 +2,24 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class Adminsite::AdminApplicationController < ActionController::Base
-  include ::CanCan::ControllerAdditions
   helper :all, "admin/adminsite_application"
   layout 'adminsite/admin'
+
   before_filter :authenticate_adminsite_admin_user!
-  authorize_resource
-  # check_authorization
+
+  include ::CanCan::ControllerAdditions
+  authorize_resource class: :resource_class
+  check_authorization
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to admin_root_url, :alert => exception.message
   end
 
   protected
+
+  def resource_class
+    self
+  end
 
   def current_ability
     @current_ability ||= ::Adminsite::Ability.new(current_adminsite_admin_user)
