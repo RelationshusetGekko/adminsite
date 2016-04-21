a86d29a1399f18304d1d17d4d61a06aacee67dc8
Bo Frederiksen <bof@rhg.dk>
2016-04-21 13:59:34 +0200

    Fixed assets for sprockets 3.0+ and menu template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths += %w( adminsite/admin )
+Rails.application.config.assets.paths |= Dir.glob("#{Adminsite::Engine.root}/app/assets/*" )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( admin.css admin.js )
+Rails.application.config.assets.precompile |= %w( admin.css admin.js )
+Rails.application.config.assets.precompile |= %w( adminsite/admin/**/* adminsite/admin/* )
